91456128cf291b261fec94cdf5d2dd36881d803d
yjk <kyj8028@hotmail.com>
2020-03-15 22:44:42 +0900

    #2.0.1 Image Field Update
    이미지 업로드 관련 models, admin update

--- main/admin.py
+++ main/admin.py
@@ -1,14 +1,39 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 from .models import Post, Member
 
 
 class PostAdmin(admin.ModelAdmin):
     list_display = (
         'title',
+        'title_length',
         'contents',
         'writer',
         'created',
+        'photo_tag'
         )
+
+    def photo_tag(self, post):
+        if post.photo:
+            return mark_safe(f'<img src="{post.photo.url}" style="width:72px;" />')
+        return None
+
+    # list_display_links = (
+    #     'title',
+    # )
+
+    search_fields = (
+        'title',
+        'contents',
+    )
+
+    list_filter = (
+        'title',
+        'contents',
+    )
+
+    def title_length(self, post):
+        return len(post.title) #  연습용
 
 
 class MemberAdmin(admin.ModelAdmin):
--- main/models.py
+++ main/models.py
@@ -1,3 +1,5 @@
+import os
+from uuid import uuid4
 from django.db import models
 from django.utils import timezone
 from datetime import datetime
@@ -8,11 +10,29 @@ class Post(models.Model):
     contents = models.TextField(null=True)
     writer = models.CharField(max_length=30, null=True)
     created = models.DateField(default=timezone.now(), null=True)
+    photo = models.ImageField(blank=True, upload_to='post/%Y/%m/%d') # upload_to는 문자열 또는 함수 반환
     # category tag로 코딩
     # photo = models.ImageField()
     
     def __str__(self):
         return self.title
+
+    # uuid를 통한 이미지 파일 저장하기 (파일명 정하기)
+    # uuid4.hex 를 실행하면 32자리 난수를 만들어준다
+    def uuid_name_upload_to(instance, filename):
+        app_label = instance.__class__._meta.app_label  # 앱 별로
+        acls_name = instance.__class__.__name__.lower() # 모델 별로
+        ymd_path = timezone.now().strftime('%Y/%m/%d')  # 업로드하는 년/월/일 별로
+        uuid_name = uuid4().hex
+        extension = os.path.splitext(filename)[-1].lower()  # 확장자 추출하고 소문자로 변환
+        return '/'.join([
+            app_label,
+            acls_name,
+            ymd_path,
+            uuid_name[:2],
+            uuid_name + extension,
+        ])
+
 
 class Member(models.Model):
     name = models.CharField(max_length=50, null=True)
